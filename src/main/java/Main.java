0f028a6bf179e43f9a8924ffa70056721ee1bfb2
symoreios <jesse.valverde@outlook.com>
2024-12-25 23:00:42 -0800

    3rd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -6,6 +8,7 @@ public class Main {
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
+    String response = "HTTP/1.1 200 OK\r\n\r\n";
 
     // Uncomment this block to pass the first stage
 
@@ -16,8 +19,13 @@ public class Main {
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
 
-       serverSocket.accept(); // Wait for connection from client.
+       Socket client = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
+       OutputStream outputStream = client.getOutputStream();
+       PrintWriter writer = new PrintWriter(outputStream, true);
+
+       writer.println(response);
+       writer.flush();
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
